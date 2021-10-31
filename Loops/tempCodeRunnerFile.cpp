b7d0a309171d383cfa7fdb1b1a1fd1036fa82e55
farish <developer.websites1@gmail.com>
2021-10-31 22:03:48 +0530

    pattern

--- Loops/tempCodeRunnerFile.cpp
+++ Loops/tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-cout<<endl;
+for(int i=n; i>=1; i--){
+    //     for(int j=1; j<=n-i;j++){
+    //         cout<<" ";
+    //     }
+    //     for(int j=1; j<=2*i-1;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
