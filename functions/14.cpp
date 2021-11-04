f5c2304b6207363945688b9fe7a17aa1a33fa7b7
farish <developer.websites1@gmail.com>
2021-11-04 14:56:23 +0530

    functions

--- functions/14.cpp
+++ functions/14.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 #include<cmath>
 using namespace std;
 
+// FIRST APPROACH : 
 void pythagorean_triplates(int x, int y, int z){
   int a ,b,c;
   if(x > y){
@@ -19,6 +20,7 @@ void pythagorean_triplates(int x, int y, int z){
   }else{
     c =z;a = y;b = x;
   }
+
   int leftSide = (float) pow(a, 2) + (float) pow(b, 2);
   int rightside = (float) pow(c, 2);
   if(leftSide == rightside){
@@ -28,8 +30,40 @@ void pythagorean_triplates(int x, int y, int z){
     }
 }
 
+// SECOND APPROACH : 
+
+// bool pythagorean_triplates(int x, int y, int z){
+//   int a ,b,c;
+//   c = max(x, max(y,z));
+//   if(c == x){
+//     b = y;
+//     a = z;
+//   }else if(c == y){
+//     b = x;
+//     a = z;
+//   }else{
+//     b = x;
+//     a = y;
+//   }
+//   int leftSide = (float) pow(a, 2) + (float) pow(b, 2);
+//   int rightside = (float) pow(c, 2);
+   
+//    if(leftSide == rightside){
+//       return true;
+//     }else{
+//       return false;
+//     }
+// }
+
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
      pythagorean_triplates(a,b,c);
+
+    // SECOND APPROACH : 
+    //  if(pythagorean_triplates(a,b,c)){
+    //     cout<<"Pythagorean triplates";
+    //  }else{
+    //     cout<<"Not Pythagorean triplates";
+    //  }
 }
