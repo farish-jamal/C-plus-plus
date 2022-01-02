b05a41a94bd7926353ffc05337bd1ad30388f82f
farish <developer.websites1@gmail.com>
2022-01-03 02:09:14 +0530

    dfh

--- DSA Love Babbar/Strings/pattern.cpp
+++ DSA Love Babbar/Strings/pattern.cpp
@@ -5,26 +5,23 @@ int main(){
  int n;
  cin >> n;
 
- for (int i = 1; i <= n; i++) {
-  for (int j = 1; j < i; j++){
-    cout << " ";
+  for(int i=n; i>1; i--){
+   for(int j=1; j<=n-i;j++){
+    cout<<"  ";
   }
-  for (int k = i; k <= n; k++){
-   cout << "*" << " "; 
-  }      
-  cout << endl;
- }
-
- // Neeche wala part
- for (int i = n-1; i >= 0; i--) {
-  for (int j = 1; j < i; j++){
-    cout << " ";
+  for(int j=1; j<=2*i-1;j++){
+   cout<<"* ";
   }
-  for (int k = i; k <= n; k++){
-   cout << "*" << " "; 
-  }      
-  cout << endl;
+  cout<<endl;
+  }
+  for(int i=1; i<=n; i++){
+   for(int j=1; j<=n-i;j++){
+    cout<<"  ";
+  }
+  for(int j=1; j<=2*i-1;j++){
+   cout<<"* ";
+  }
+  cout<<endl;
  }
- 
  return 0;
 }
